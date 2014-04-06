d5512c0ec6b42b7678b794588aec39000f3b2f73
ArkhamNetwork Dev <devane1249@Gmail.com>
2014-04-06 23:06:45 +0100

    Didnt realise that events are async by default.

--- src/org/arkhamnetwork/uuid/listener/LoginListener.java
+++ src/org/arkhamnetwork/uuid/listener/LoginListener.java
@@ -23,37 +23,34 @@ public class LoginListener implements Listener {
 
     @EventHandler
     public void onLogin(final LoginEvent event) {
-        plugin.getProxy().getScheduler().runAsync(plugin, new Runnable() {
-            @Override
-            public void run() {
-                String playerName = event.getConnection().getName();
-                String UUID = event.getConnection().getUUID();
-                try {
-                    if (playerName == null || UUID == null || plugin.DB == null || plugin.C == null || plugin.C.isClosed()) {
-                        event.setCancelReason(ChatColor.RED + "We could not validate your UUID with our server.");
-                        event.setCancelled(true);
-                        return;
-                    }
-
-                    StorageManager.checkAndCreatePlayer(playerName, UUID);
-
-                    if (StorageManager.playerHasChangedName(playerName, UUID)) {
-                        String oldAccountName = plugin.DB.getStoredAccountNameFromUUID(UUID);
-                        event.setCancelReason(ChatColor.AQUA + "It appears as if you have changed your account name. Please change your account name back to " + ChatColor.GOLD + oldAccountName + ChatColor.AQUA + " or we cannot let you login. We do not support account name changing yet.");
-                        event.setCancelled(true);
-                    }
-
-                    if (StorageManager.playerNameHasChangedFromUUID(UUID, playerName)) {
-                        String oldUUID = plugin.DB.getStoredUUIDFromAccount(playerName);
-                        event.setCancelReason(ChatColor.AQUA + "It appears that this account isnt yours. Only the player with the UUID " + ChatColor.RED + oldUUID + ChatColor.AQUA + " can login with this username. Your UUID is " + ChatColor.RED + UUID);
-                        event.setCancelled(true);
-                    }
-                } catch (SQLException ex) {
-                    event.setCancelReason(ChatColor.RED + "We could not validate your UUID with our server.");
-                    event.setCancelled(true);
-                }
+        String playerName = event.getConnection().getName();
+        String UUID = event.getConnection().getUUID();
+        try {
+            if (playerName == null || UUID == null || plugin.DB == null || plugin.C == null || plugin.C.isClosed()) {
+                event.setCancelReason(ChatColor.RED + "We could not validate your UUID with our server.");
+                event.setCancelled(true);
+                return;
             }
-        });
+
+            StorageManager.checkAndCreatePlayer(playerName, UUID);
+
+            if (StorageManager.playerHasChangedName(playerName, UUID)) {
+                String oldAccountName = plugin.DB.getStoredAccountNameFromUUID(UUID);
+                event.setCancelReason(ChatColor.AQUA + "It appears as if you have changed your account name. Please change your account name back to " + ChatColor.GOLD + oldAccountName + ChatColor.AQUA + " or we cannot let you login. We do not support account name changing yet.");
+                event.setCancelled(true);
+                return;
+            }
+
+            if (StorageManager.playerNameHasChangedFromUUID(UUID, playerName)) {
+                String oldUUID = plugin.DB.getStoredUUIDFromAccount(playerName);
+                event.setCancelReason(ChatColor.AQUA + "It appears that this account isnt yours. Only the player with the UUID " + ChatColor.RED + oldUUID + ChatColor.AQUA + " can login with this username. Your UUID is " + ChatColor.RED + UUID);
+                event.setCancelled(true);
+                return;
+            }
+        } catch (SQLException ex) {
+            event.setCancelReason(ChatColor.RED + "We could not validate your UUID with our server.");
+            event.setCancelled(true);
+        }
     }
 
 }
